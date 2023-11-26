e52d4e01b4f9cfe04bd6ff706dbaaab80e389851
HarveyPost <146440092+HarveyPost@users.noreply.github.com>
2023-11-26 18:44:08 +0000

    Coursework2 finished

--- Coursework2/StepCounter.c
+++ Coursework2/StepCounter.c
@@ -3,6 +3,33 @@
 #include <string.h>
 #include "FitnessDataStruct.h"
 
+// This is your helper function. Do not change it in any way.
+// Inputs: character array representing a row; the delimiter character
+// Ouputs: date character array; time character array; steps character array
+void tokeniseRecord(const char *input, const char *delimiter,
+                    char *date, char *time, char *steps) {
+    // Create a copy of the input string as strtok modifies the string
+    char *inputCopy = strdup(input);
+    
+    // Tokenize the copied string
+    char *token = strtok(inputCopy, delimiter);
+    if (token != NULL) {        strcpy(date, token);
+    }
+    
+    token = strtok(NULL, delimiter);
+    if (token != NULL) {
+        strcpy(time, token);
+    }
+    
+    token = strtok(NULL, delimiter);
+    if (token != NULL) {
+        strcpy(steps, token);
+    }
+    
+    // Free the duplicated string
+    free(inputCopy);
+}
+
 char get_filename(char *filename)
 {
     printf("Input filename: ");
